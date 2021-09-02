cdd1ea6d0ca544e9d93d5ced5330a4629ce8e996
tsubedy <tsubedy@gmail.com>
2021-09-02 03:54:51 -0700

    Final push

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import sqlalchemy
 from flask import Flask, jsonify
 import datetime as dt
@@ -6,7 +5,6 @@ import numpy as np
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
-
 
 # Database Setup
 # create engine to hawaii.sqlite
@@ -28,7 +26,6 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-
 # Flask Setup
 app = Flask(__name__)
 
@@ -37,64 +34,78 @@ app = Flask(__name__)
 def main():
     return (
         f"Welcome to the Climate App Home Page!<br>"
-        f"Available Routes Below:<br>"
+        f"Available Routes:<br>"
         f"/api/v1.0/precipitation<br>"
         f"/api/v1.0/stations<br>"
         f"/api/v1.0/tobs<br>"
         f"/api/v1.0/<start><br>"
         f"/api/v1.0/<start>/<end><br>"
     )
-        
     
 @app.route("/api/v1.0/precipitation")
 def precip():
-
-    # Design a query to retrieve the last 12 months of precipitation data and plot the results
-    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-
-    # Get the first element of the tuple
-    most_recent_date = most_recent_date[0]
-    # most_recent_date
-
     prev_year = dt.date(2017,8,23)- dt.timedelta(days=365)
-
-    # Perform a query to retrieve the data and precipitation scores
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).\
+                order_by(Measurement.date).all()
     result_dict = dict(results)
-
-    # return json list of dictionary
+    session.close()
     return jsonify(result_dict)
-
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Design a query to find the most active stations (i.e. what stations have the most rows?)
-    # List the stations and the counts in descending order.
-
     stations = session.query(Measurement.station, func.count(Measurement.id)).\
             group_by(Measurement.station).order_by(func.count(Measurement.id).desc()).all()
 
-    # convert results to a dict
     stations_dict = dict(stations)
-
-    # return json list of dict
+    session.close()
     return jsonify(stations_dict)
-
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-
     max_temp_obs = session.query(Measurement.station, Measurement.tobs).\
         filter(Measurement.date >= '2016-08-23').all()
 
-    # convert results to dict
     tobs_dict = dict(max_temp_obs)
-
-    # return json list of dict
+    session.close()
     return jsonify(tobs_dict)
 
 
-  
+@app.route("/api/v1.0/<start>")
+def start(start):
+    result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
+                                func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+    
+    session.close()
+    tobsall = []
+
+    for min,avg,max in result:
+        tobs_dict = {}
+        tobs_dict["Min"] = min
+        tobs_dict["Average"] = avg
+        tobs_dict["Max"] = max
+        tobsall.append(tobs_dict)
+        
+    return jsonify(tobsall)
+
+@app.route('/api/v1.0/<start>/<end>')
+def start_end(start,end):
+    queryresult = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
+                func.max(Measurement.tobs)).filter(Measurement.date >= start).\
+                filter(Measurement.date <= end).all()
+
+    session.close()
+
+    tobsall = []
+    for min,avg,max in queryresult:
+        tobs_dict = {}
+        tobs_dict["Min"] = min
+        tobs_dict["Average"] = avg
+        tobs_dict["Max"] = max
+        tobsall.append(tobs_dict)
+
+    return jsonify(tobsall)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
